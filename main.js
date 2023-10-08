af80a72aa346d10003c09fd9cd18b4582551a77b
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-10-08 03:28:51 +0200

    select a file button

--- main.js
+++ main.js
@@ -1,21 +1,70 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs');
 
-// Create a new browser window
+// Create a reference to the main window
+let mainWindow;
+
+// Create a new browser window function
 function createWindow() {
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
     },
+    icon: path.join(__dirname, 'icon.png'),
   });
 
-  // Load an HTML file into the window
-  win.loadFile('index.html');
+  mainWindow.loadFile('index.html');
+
+  // Handle the "Select a File" button click
+  mainWindow.webContents.on('did-finish-load', () => {
+    const selectFileButton = mainWindow.webContents.document.getElementById('select-file');
+
+    selectFileButton.addEventListener('click', () => {
+      openFile();
+    });
+  });
+
+  // ...rest of your code...
 }
 
-// Create the main window when the app is ready
+// Function to open a file dialog
+function openFile() {
+  const options = {
+    properties: ['openFile'], // Limit to opening a single file
+    filters: [
+      { name: 'Text Files', extensions: ['txt', 'md'] }, // Allow .txt and .md files
+      { name: 'All Files', extensions: ['*'] }, // Allow all file types
+    ],
+  };
+
+  dialog.showOpenDialog(mainWindow, options).then((result) => {
+    if (!result.canceled) {
+      const filePath = result.filePaths[0];
+
+      // Use Node.js fs module to read the selected file
+      fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+          console.error('Error reading the file:', err);
+          return;
+        }
+
+        // Determine the file type based on the file extension
+        const fileType = path.extname(filePath);
+
+        // Send the file content to the renderer process
+        mainWindow.webContents.send('open-file', { content: data, fileType });
+      });
+    }
+  });
+}
+
+// ...rest of your code...
+
 app.whenReady().then(createWindow);
+
 
 // Quit the app when all windows are closed (except on macOS)
 app.on('window-all-closed', () => {
@@ -30,4 +79,3 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
