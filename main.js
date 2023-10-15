dd1b4ce5e6801fe7a804f3032ff9defb2a6478b7
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-10-16 00:43:11 +0200

    window opener

--- main.js
+++ main.js
@@ -2,7 +2,24 @@ const { app, BrowserWindow } = require('electron');
 
 let mainWindow;
 
-app.on('ready', () => {
+function createMainWindow() {
     mainWindow = new BrowserWindow({ width: 800, height: 600 });
     mainWindow.loadFile('index.html');
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createMainWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createMainWindow();
+    }
 });
