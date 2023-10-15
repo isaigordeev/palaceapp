c3ae01a60f91f556a5d5dab8acace23a16ae9d87
Isai Gordeev <isai.gordeev.2022@polytechnique.org>
2023-10-16 00:46:42 +0200

    panel opener

--- renderer.js
+++ renderer.js
@@ -1,40 +1,13 @@
-const fs = require('fs');
-const { dialog } = require('electron').remote;
+const openPanelButton = document.getElementById('openPanelButton');
+const closePanelButton = document.getElementById('closePanelButton');
+const panel = document.getElementById('panel');
 
-document.getElementById('saveButton').addEventListener('click', () => {
-    const noteTitle = document.getElementById('noteTitle').value;
-    const noteContent = document.getElementById('noteContent').value;
-
-    dialog.showSaveDialog((filePath) => {
-        if (filePath) {
-            const noteData = JSON.stringify({ title: noteTitle, content: noteContent });
-
-            fs.writeFile(filePath, noteData, (err) => {
-                if (err) {
-                    console.error('Error saving note:', err);
-                } else {
-                    console.log('Note saved successfully.');
-                }
-            });
-        }
-    });
+openPanelButton.addEventListener('click', () => {
+    panel.style.right = '0'; // Slide the panel in from the right
+    panel.classList.remove('hidden');
 });
 
-document.getElementById('loadButton').addEventListener('click', () => {
-    dialog.showOpenDialog((filePaths) => {
-        if (filePaths && filePaths.length > 0) {
-            const filePath = filePaths[0];
-
-            fs.readFile(filePath, 'utf-8', (err, data) => {
-                if (err) {
-                    console.error('Error loading note:', err);
-                } else {
-                    const note = JSON.parse(data);
-                    document.getElementById('noteTitle').value = note.title;
-                    document.getElementById('noteContent').value = note.content;
-                    console.log('Note loaded successfully.');
-                }
-            });
-        }
-    });
+closePanelButton.addEventListener('click', () => {
+    panel.style.right = '-300px'; // Slide the panel out to the right
+    panel.classList.add('hidden');
 });
